bb63062c5ae687a5e8e38f3c53e60a846588f5ea
Stevan Sandi <stevan.sandi@udg.edu.me>
2018-05-09 12:25:04 +0200

    Midterm 02 Tasks

---
+++ task1.py
@@ -0,0 +1,35 @@
+"""
+===================   TASK 1   ====================
+* Name: Caesar Cipher
+*
+* Caesar Cipher is encryption technique in which
+* each letter in the plaintext is replaced by a
+* letter some fixed number of positions down the
+* alphabet. The method is named after Julius Caesar,
+* who used it in his private correspondence.
+*
+* Write a script that will accept number of positions
+* that should be shifted down the Unicode code points
+* of character. White spaces and punctuation marks
+* should be ignored during encryption process.
+*
+* Hint: `ord()` returns integer representing Unicode
+* code point for single character.
+*
+* Note: Please describe in details possible cases
+* in which your solution might not work.
+*
+* Use main() function to test your solution.
+===================================================
+"""
+
+# Write your functions here
+
+
+
+def main():
+    # Test your functions here
+    pass
+
+if __name__ == "__main__":
+    main()
---
+++ task2.py
@@ -0,0 +1,16 @@
+"""
+===================   TASK 2   ====================
+* Name: Roll The Dice
+*
+* Write a script that will simulate rolling the
+* dice. The script should fetch the number of times
+* the dice should be "thrown" as user input.
+* At the end, the script should print how many times
+* each number appeared (1 - 6).
+*
+* Note: Please describe in details possible cases
+* in which your solution might not work.
+===================================================
+"""
+
+# Write your script here
